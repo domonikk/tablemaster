1f45be0ba4df3e1ff0b8abbb3d983d37f8f7491e
domonikk <45376672+domonikk@users.noreply.github.com>
2025-07-16 18:55:00 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-    variable: "--font-inter",
-    subsets: ["latin"],
-    display: "swap",
-    axes: ["opsz"],
-});
 
 export const metadata: Metadata = {
     title: "Table Masters Brand Guideline",
@@ -22,7 +15,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
+                className={` font-sans antialiased bg-neutral-950 text-white`}
             >
                 {children}
             </body>
