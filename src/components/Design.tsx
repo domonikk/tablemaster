de16a336847b6129f0e10c3e3114b532d0731a0f
domonikk <45376672+domonikk@users.noreply.github.com>
2025-07-16 19:29:54 -0500

    update

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -7,7 +7,7 @@ const Design = () => {
             <div className=' col-span-1 row-span-4  md:col-span-6 md:row-span-3 bg-[#660503] rounded-3xl flex flex-col justify-center items-center text-center font-headline lg:text-9xl md:text-6xl text-3xl'>Markova Regular</div>
             <div className=' col-span-1 row-span-4 md:col-span-4 md:row-span-4 bg-colors rounded-3xl flex justify-center items-center'></div>
             <div className=' col-span-1 row-span-4 md:col-span-3 md:row-span-2 bg-logos rounded-3xl flex justify-center items-center'></div>
-            <div className=' col-span-1 row-span-4 md:col-span-3 md:row-span-4 bg-header bg-poster rounded-3xl flex justify-center items-center'></div>
+            <div className=' col-span-1 row-span-4 md:col-span-3 md:row-span-4 bg-poster rounded-3xl flex justify-center items-center'></div>
             <div className=' col-span-1 row-span-4 md:col-span-4 md:row-span-3 bg-vid rounded-3xl flex justify-center items-center'></div>
             <div className=' col-span-1 row-span-4 md:col-span-3 md:row-span-2 bg-img1 rounded-3xl flex justify-center items-center'></div>
         </div>
